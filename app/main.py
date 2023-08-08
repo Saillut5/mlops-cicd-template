dfe727249037780410d9e54f3c73c77131cbf417
Silas Vance <phosubvieworsi@gmx.net>
2023-08-08 18:14:00 -0400

    feat: Add environment variable support

--- app/main.py
+++ app/main.py
@@ -157,3 +157,4 @@ async def predict(request: PredictionRequest):
 # Simulated change on 2023-07-31 12:18:00
 # Simulated change on 2023-08-04 12:32:00
 # Simulated change on 2023-08-08 16:33:00
+# Simulated change on 2023-08-08 18:14:00
