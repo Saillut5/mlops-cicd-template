0a8c4ee9b92df5829e7aeaff1bcd87682665def0
Silas Vance <phosubvieworsi@gmx.net>
2023-12-07 16:51:00 -0500

    test: Add integration tests for full pipeline

--- app/main.py
+++ app/main.py
@@ -176,3 +176,4 @@ async def predict(request: PredictionRequest):
 # Simulated change on 2023-11-20 12:36:00
 # Simulated change on 2023-11-23 09:22:00
 # Simulated change on 2023-12-05 16:03:00
+# Simulated change on 2023-12-07 16:51:00
